30334b0954afecc60c3873ab72419a946491ca57
Stavros <stavros.denaxas@gmail.com>
2021-02-07 11:55:12 +0000

    added title, menu and game screen framework and transitions

--- gamestate.py
+++ gamestate.py
@@ -3,12 +3,48 @@ import pygame
 
 class Gamestate():
     def __init__(self):
-        self.state = 'main_game'
+        self.state = 'title_screen'
         # set resolution variables
         self.screen_width = 1920
         self.screen_height = 1080
         self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
         self.running = True
+
+    def title_screen(self):
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                self.state = "main_menu"
+
+        # Fill the background with white
+        self.screen.fill((255, 255, 255))
+
+        # Draw a solid blue circle in the center
+        pygame.draw.circle(self.screen, (0, 0, 255), (750, 750), 75)
+
+        # Flip the display
+        pygame.display.flip()
+
+
+    def main_menu(self):
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                self.state = "main_game"
+
+        # Fill the background with white
+        self.screen.fill((255, 0, 255))
+
+        # Draw a solid blue circle in the center
+        pygame.draw.circle(self.screen, (0, 0, 255), (500, 500), 75)
+
+        # Flip the display
+        pygame.display.flip()
+
 
     def main_game(self):
         # Did the user click the window close button?
--- mainloop.py
+++ mainloop.py
@@ -10,7 +10,13 @@ game_state = gamestate.Gamestate()
 
 # Run until the user asks to quit
 while game_state.running:
-    game_state.main_game()
+
+    if game_state.state == 'title_screen':
+        game_state.title_screen()
+    if game_state.state == 'main_menu':
+        game_state.main_menu()
+    if game_state.state == 'main_game':
+        game_state.main_game()
     clock.tick(60)
 
 # Done! Time to quit.
