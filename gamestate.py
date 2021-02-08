ae4c04e655b9047445c4c41f9755a4aecae58d01
Stavros <stavros.denaxas@gmail.com>
2021-02-08 16:21:40 +0000

    Added Text title in title screen Added Font folder and Oswald-Bold.ttf font

--- gamestate.py
+++ gamestate.py
@@ -1,7 +1,8 @@
 import pygame
+import pygame.freetype
 
 
-class Gamestate():
+class Gamestate:
     def __init__(self):
         self.state = 'title_screen'
         # set resolution variables
@@ -9,6 +10,7 @@ class Gamestate():
         self.screen_height = 1080
         self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
         self.running = True
+        self.GAME_FONT = pygame.freetype.Font("Fonts/Oswald-Bold.ttf", 128)
 
     def title_screen(self):
         # Did the user click the window close button?
@@ -19,14 +21,14 @@ class Gamestate():
                 self.state = "main_menu"
 
         # Fill the background with white
-        self.screen.fill((255, 255, 255))
-
+        self.screen.fill((0, 0, 0))
+        self.GAME_FONT.render_to(self.screen,
+                                 (round(self.screen_width / 2 - 0.2 * self.screen_width),
+                                  round(self.screen_height / 2 - 0.1 * self.screen_width)),
+                                 "Into the Dark",
+                                 (50, 50, 50))
         # Draw a solid blue circle in the center
-        pygame.draw.circle(self.screen, (0, 0, 255), (750, 750), 75)
-
-        # Flip the display
         pygame.display.flip()
-
 
     def main_menu(self):
         # Did the user click the window close button?
@@ -37,14 +39,13 @@ class Gamestate():
                 self.state = "main_game"
 
         # Fill the background with white
-        self.screen.fill((255, 0, 255))
+        self.screen.fill((0, 0, 0))
 
         # Draw a solid blue circle in the center
         pygame.draw.circle(self.screen, (0, 0, 255), (500, 500), 75)
 
         # Flip the display
         pygame.display.flip()
-
 
     def main_game(self):
         # Did the user click the window close button?
@@ -59,9 +60,3 @@ class Gamestate():
 
         # Flip the display
         pygame.display.flip()
-
-
-
-
-
-
